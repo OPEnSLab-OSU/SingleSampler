de12bb3634765ac8676bc8df2f3a9fc652389670
Liam Zimmermann <zimmerml@oregonstate.edu>
2020-12-15 12:58:16 -0800

    change factor and offset for the load cell

--- src/Components/LoadCell.hpp
+++ src/Components/LoadCell.hpp
@@ -7,8 +7,8 @@ public:
 	Adafruit_ADS1115 ads;
 	int32_t tare;
 
-	const float factor = 0.5353;
-	const int offset   = 10402;
+	const float factor = 0.5539;
+	const int offset   = 10944;
 	LoadCell(const char * name, KPController * controller)
 		: KPComponent(name, controller), ads(0x48) {}
 	void setup() override {
