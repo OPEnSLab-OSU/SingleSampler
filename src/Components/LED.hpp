d9569e1f5f101296793f3350a0485f79d92abc6a
Liam Zimmermann <zimmerml@oregonstate.edu>
2020-12-21 02:37:31 -0800

    LED priority system testing for Hadi

--- src/Components/LED.hpp
+++ src/Components/LED.hpp
@@ -3,17 +3,42 @@
 #include <KPFoundation.hpp>
 #include <Application/Constants.hpp>
 #include <Adafruit_NeoPixel.h>
+#include <array>
+#include <map>
+
+struct Light {
+	Light(unsigned short priority, unsigned short r, unsigned short b, unsigned short g)
+		: priority(priority), r(r), b(b), g(g){};
+	unsigned short priority;
+	unsigned short r;
+	unsigned short b;
+	unsigned short g;
+};
+
+namespace LEDNames {
+	constexpr const char * IDLE	   = "idle";
+	constexpr const char * RUN	   = "run";
+	constexpr const char * BATTERY = "battery";
+};	// namespace LEDNames
+
 class LED : public KPComponent {
 public:
 	unsigned short r;
 	unsigned short g;
 	unsigned short b;
 	Adafruit_NeoPixel pixel;
+	const unsigned short no_levels		 = 3;
+	std::array<Light *, 3> lights_active = {nullptr, nullptr, nullptr};
+	std::map<const char *, Light> lights;
 	LED(const char * name, KPController * controller)
 		: KPComponent(name, controller), pixel(1, TPICDevices::PIXEL, NEO_RGB + NEO_KHZ800) {}
 	void setup() {
 		pixel.begin();
 		setIdle();
+
+		lights.at(LEDNames::IDLE)	 = Light(2, 0, 30, 0);
+		lights.at(LEDNames::RUN)	 = Light(2, 0, 30, 0);
+		lights.at(LEDNames::BATTERY) = Light(0, 30, 0, 0);
 	}
 
 	void setColor(unsigned short nr, unsigned short ng, unsigned short nb) {
@@ -23,6 +48,25 @@ public:
 		b = nb;
 		pixel.setPixelColor(0, g, r, b);
 		pixel.show();
+	}
+
+	void refreshLights() {
+		bool finished = false;
+		for (int i = 0; i < no_levels && false == finished; ++i) {
+			if (nullptr != lights_active.at(i)) {
+				setColor(lights_active.at(i)->r, lights_active.at(i)->g, lights_active.at(i)->b);
+				finished = true;
+			}
+		}
+	}
+
+	void setLight(Light * lt) {
+		lights_active.at(lt->priority) = lt;
+		refreshLights();
+	}
+
+	void setLight(const char * name) {
+		setLight(&(lights.at(name)));
 	}
 
 	void setRun() {
