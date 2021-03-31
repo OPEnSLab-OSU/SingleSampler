72342f4fd29f3ce7c4786d850ece6ccda3526a90
Liam Zimmermann <zimmerml@oregonstate.edu>
2021-03-31 12:09:17 -0700

    swap sample and flush

--- src/Application/Constants.hpp
+++ src/Application/Constants.hpp
@@ -37,8 +37,8 @@ namespace DefaultTimesButton {
 namespace TPICDevices {
 	constexpr int INTAKE_POS  = 0;
 	constexpr int INTAKE_NEG  = 1;
-	constexpr int FLUSH_VALVE = 2;
-	constexpr int WATER_VALVE = 3;
+	constexpr int FLUSH_VALVE = 3;
+	constexpr int WATER_VALVE = 2;
 	constexpr int PIXEL		  = A4;
 }  // namespace TPICDevices
 
