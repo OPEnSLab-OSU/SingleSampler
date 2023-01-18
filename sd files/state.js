c2e1abd4e3c28796b994a5531c27da17c37754ce
Cara Walter <cara.walter@oregonstate.edu>
2023-01-18 12:54:15 -0800

    Sampler 1 state.js file contents

--- sd files/state.js
+++ sd files/state.js
@@ -1,16 +1,1 @@
-{
-	"sample" : {
-		"flush_time" : 50,
-		"sample_time" : 60,
-		"idle_time" : 3490,
-		"setup_time" : 0,
-		"last_cycle" : 24,
-		"sample_mass" : 100
-	},
-	"clean" : {
-		"sample_time" : 5,
-		"idle_time" : 0,
-		"flush_time" : 20,
-		"last_cycle" : 24
-	}
-}
+{"sample":{"last_cycle":24,"idle_time":0,"flush_time":5,"sample_time":300,"setup_time":0,"sample_mass":2000},"load_cell":{"offset":-19857.15,"factor":0.002348},"pressure":{"max_pressure":2000,"min_pressure":0}}
